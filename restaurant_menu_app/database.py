2d6fa6a6b903a31eb68b60a922bd0bde6f7ff58d
Sergey Kim <sergkim7@gmail.com>
2023-01-18 12:48:54 +0300

    fix syntax error in database.py. All test passed

--- restaurant_menu_app/database.py
+++ restaurant_menu_app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS})@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
